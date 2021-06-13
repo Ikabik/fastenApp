07aff2499cdbb5ded5973e2ae96fcd4a149c7c70
Steff <stefanie.weiss@posteo.de>
2021-06-13 20:27:08 +0200

    Button to stop count, some styling

--- source.js
+++ source.js
@@ -1,57 +1,51 @@
 'use strict'
 
-/*let todoModel = {
-
-    items: ko.observableArray([]),
-
-    addItem: function (){
-        let model = this;
-        let item = {
-            text: ko.observable(text.value),
-            priority: ko.observable(priority.value)
-        }
-
-        model.items.push(item)
-    },
-
-    removeItem: function (item){
-        let model = this;
-        model.items.remove(item);
-    }
-};
-
-ko.applyBindings(todoModel);*/
 let Counter = {
 
-        sec: ko.observable(0),
-        min: ko.observable(0),
-        h: ko.observable(0),
+    sec: ko.observable(0),
+    min: ko.observable(0),
+    h: ko.observable(0),
+    buttonClicked: false,
+
 
     counterFunctionality: function () {
         let Counter = this;
+        let startStop; 
 
-        function count() {
-            let i = Counter.sec()
-            i++
-            Counter.sec(i)
+        if (this.buttonClicked === false) {
+            function count() {
+                let i = Counter.sec()
+                i++
+                Counter.sec(i)
 
-            if (Counter.sec() === 60) {
-                let j = Counter.min()
-                j++
-                Counter.min(j)
+                if (Counter.sec() === 60) {
+                    let j = Counter.min()
+                    j++
+                    Counter.min(j)
 
-                Counter.sec(0)
+                    Counter.sec(0)
+                }
+                if (Counter.min() === 60) {
+                    let k = Counter.h()
+
+                    k++
+                    Counter.h(k)
+                    Counter.min(0)
+                }
+                console.log(Counter.sec(), Counter.min())
             }
-            if (Counter.min() === 60) {
-                let k = Counter.h()
-
-                k++
-                Counter.h(k)
-                Counter.min(0)
-            }
-            console.log(Counter.sec(), Counter.min())
+            this.buttonClicked = true
+            this.startStop= setInterval(count, 1000)
+            document.getElementById('startEnd').textContent = 'Fasten beenden?'
+            console.log('Hello')
         }
-        setInterval(count, 1000)
+        else {
+            clearInterval(this.startStop)
+            this.buttonClicked = false
+            document.getElementById('startEnd').textContent = 'Fasten starten!'
+            console.log ('Noch mal')
+        }
+
     }
 
 }
